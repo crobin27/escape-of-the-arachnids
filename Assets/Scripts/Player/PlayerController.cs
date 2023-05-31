e38dff7a5c7da1e79b33c7a3e751edecf24cc2c6
crobin27 <crobin27@calpoly.edu>
2023-05-30 21:50:10 -0700

    refactored player

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,113 +3,103 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayerController : MonoBehaviour, IDamageable
+namespace Player
 {
-    public static PlayerController Instance { get; private set; }
-
-    [SerializeField] private int m_health = 5;
-    [SerializeField] private int m_maxHealth = 5;
-    [SerializeField] private float m_speed = 10f;
-    [SerializeField] private float m_normalSpeed = 10f;
-
-    [SerializeField] private float m_attackRange = 2.0f;
-    [SerializeField] private int m_attackDamage = 10;
-    
-    public int Health
+    public class PlayerController : MonoBehaviour, IDamageable
     {
-        get { return m_health; }
-        set { m_health = value; }
-    }
+        public static PlayerController Instance { get; private set; }
+        [SerializeField] private int m_health = 5;
+        [SerializeField] private int m_maxHealth = 5;
+        [SerializeField] private float m_speed = 10f;
+        [SerializeField] private float m_normalSpeed = 10f;
+        [SerializeField] private float m_attackRange = 2.0f;
+        [SerializeField] private int m_attackDamage = 10;
+        private Animator m_animator;
+        [SerializeField] private Rigidbody2D m_rb;
+        public VariableJoystick joystick;
+        private const float moveLimiter = 0.7f;
+        private float horizontal = 0;
+        private float vertical = 0;
 
-    public int MaxHealth
-    {
-        get { return m_maxHealth; }
-    }
-    public float Speed
-    {
-        get{ return m_speed; }
-        set { m_speed = value; }
-    }
-    public float NormalSpeed
-    {
-        get { return m_normalSpeed; }
-        set { m_normalSpeed = value; }
-    }
-
-    private Animator m_animator;
-
-    private float horizontal = 0;
-    private float vertical = 0;
-
-    [SerializeField] private Rigidbody2D m_rb;
-
-    public VariableJoystick joystick;
-
-    float moveLimiter = 0.7f;
-
-    // Start is called before the first frame update
-    void Awake()
-    {
-        if (Instance == null)
+        public int Health
         {
-            Instance = this;
+            get { return m_health; }
+            set { m_health = value; }
         }
-        else
+        public int MaxHealth
         {
-            Destroy(gameObject);
+            get { return m_maxHealth; }
+        }
+        public float Speed
+        {
+            get { return m_speed; }
+            set { m_speed = value; }
+        }
+        public float NormalSpeed
+        {
+            get { return m_normalSpeed; }
+            set { m_normalSpeed = value; }
         }
 
-        m_rb = GetComponent<Rigidbody2D>();
-        m_animator = GetComponent<Animator>();
-    }
-
-    private void FixedUpdate()
-    {
-        if (joystick.Horizontal == 0 && joystick.Vertical == 0)
+        // establish the singleton pattern
+        void Awake()
         {
-            m_rb.velocity = Vector2.zero;
-      
-            m_animator.SetFloat("LastX", horizontal);
-            m_animator.SetFloat("LastY", vertical);
-        }
-        else
-        {
-            if (joystick.Horizontal != 0 && joystick.Vertical != 0)
+            if (Instance == null)
             {
-                // limit movement speed diagonally, so you move at 70% speed
-                horizontal = moveLimiter * joystick.Horizontal;
-                vertical = moveLimiter * joystick.Vertical;
-
-                m_animator.SetFloat("Horizontal", horizontal);
-                m_animator.SetFloat("Vertical", vertical);
+                Instance = this;
             }
-            Vector2 direction = new Vector2(horizontal, vertical).normalized;
-             // Debug.Log("joystick vector" + direction);
-            m_rb.velocity = direction * m_speed * Time.deltaTime;
-            
+            else
+            {
+                Destroy(gameObject);
+            }
+
+            m_rb = GetComponent<Rigidbody2D>();
+            m_animator = GetComponent<Animator>();
         }
 
-        m_animator.SetFloat("Speed", m_rb.velocity.magnitude);
-
-    }
-
-    public void Die()
-    {
-        Destroy(gameObject);
-        SceneManager.LoadScene("DeathScene");
-    }
-    public void TakeDamage(int damage)
-    {
-        m_health -= damage;
-        HealthBar.Instance.UpdateHealth(m_health);
-        if (m_health <= 0)
+        private void FixedUpdate()
         {
-            // Load End Scene
-            Die();
+            // stop moving if the joystick is not being touched
+            if (joystick.Horizontal == 0 && joystick.Vertical == 0)
+            {
+                m_rb.velocity = Vector2.zero;
+                // set the last direction the player was moving in to animator
+                m_animator.SetFloat("LastX", horizontal);
+                m_animator.SetFloat("LastY", vertical);
+            }
+            else
+            {
+                if (joystick.Horizontal != 0 && joystick.Vertical != 0)
+                {
+                    // limit movement speed diagonally, so you move at 70% speed
+                    horizontal = moveLimiter * joystick.Horizontal;
+                    vertical = moveLimiter * joystick.Vertical;
+
+                    m_animator.SetFloat("Horizontal", horizontal);
+                    m_animator.SetFloat("Vertical", vertical);
+                }
+
+                Vector2 direction = new Vector2(horizontal, vertical).normalized;
+                m_rb.velocity = direction * m_speed * Time.deltaTime;
+            }
+
+            m_animator.SetFloat("Speed", m_rb.velocity.magnitude);
+
+        }
+
+        public void Die()
+        {
+            SceneManager.LoadScene("DeathScene");
+        }
+
+        public void TakeDamage(int damage)
+        {
+            m_health -= damage;
+            HealthBar.Instance.UpdateHealth(m_health);
+            if (m_health <= 0)
+            {
+                Die();
+            }
         }
     }
-
-    
-
-
 }
