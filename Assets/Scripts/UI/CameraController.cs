187e4b71c8534b378d77e1cfe543b9271a53ff30
crobin27 <crobin27@calpoly.edu>
2023-05-30 22:32:00 -0700

    testing smoothdamp on camera

--- Assets/Scripts/UI/CameraController.cs
+++ Assets/Scripts/UI/CameraController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-
+    // distance fro camera to player
+    private Vector3 offset = new Vector3(0, 0, -10);
+    private float smoothSpeed = 0.125f;
+    private Vector3 velocity = Vector3.zero;
     public Transform playerTransform;
+    // private void Update()
+    // {
+    //     if (playerTransform)
+    //     {
+    //         transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, transform.position.z);
+    //     }
+    // }
+
     private void Update()
     {
-        if (playerTransform)
-        {
-            transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, transform.position.z);
-        }
+        Vector3 targetPosition = playerTransform.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothSpeed);
     }
-
-
 }
